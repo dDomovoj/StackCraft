a41037e34ea5a7a60c32422f2a8e7da28a58ff1c
Dzmitry Duleba <dmitryduleba@gmail.com>
2022-03-25 13:11:40 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,22 +6,8 @@ import PackageDescription
 let package = Package(
   name: "StackCraft",
   platforms: [SupportedPlatform.iOS(.v11)],
-  products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
-    .library(
-      name: "StackCraft",
-      targets: ["StackCraft"]),
-  ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
-  ],
-  targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
-    .target(
-      name: "StackCraft",
-      dependencies: []),
-  ],
+  products: [.library(name: "StackCraft", targets: ["StackCraft"])],
+  dependencies: [],
+  targets: [.target(name: "StackCraft", dependencies: [], path: "Sources/StackCraft")],
   swiftLanguageVersions: [.v5]
 )
